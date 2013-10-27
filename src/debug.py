ec942cde450489f0e6509b83c479bb5b7714b08f
root <root@wolfenland.workgroup>
2013-10-27 20:35:02 +0200

    - debugging tracer

--- src/debug.py
+++ src/debug.py
@@ -1,8 +1,8 @@
-
 DEBUG_SIM = False
 
 
 def traceMe():
     if DEBUG_SIM:
-        from pydevd import settrace
-        settrace(suspend = False, trace_only_current_thread = False)
+        import pydevd
+        pydevd.settrace(suspend = False)
+        pydevd.GetGlobalDebugger().setExceptHook(Exception, True, False)
