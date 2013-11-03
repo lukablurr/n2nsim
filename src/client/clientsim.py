3b5e6ad602d38cc6b9acaad5730cf8e71aedbf18
lukablurr <lukablurr@gmail.com>
2013-11-03 17:43:56 +0200

    client and server extend NodeSim

--- src/client/clientsim.py
+++ src/client/clientsim.py
@@ -2,17 +2,34 @@
 Simulated client
 '''
 
+from cmn.nodesim import NodeSim
+from client.clientcfg import ClientConfig
 from client.edge import Edge
 from client.app import App
 
 
-class ClientSim(object):
+class ClientSim(NodeSim):
     
-    def __init__(self, ip_str):
-        self.ip = ip_str
+    CONFIG = ClientConfig()
+    
+    def __init__(self, ip, port, mport):
+        NodeSim.__init__(self, ip, port, mport)
         self.edge = Edge()
-        self.app = App()
+        '''
+                edge.params.super_addr.setValue(super_addr)#TODO
+                edge.params.device_name.setValue("n2n%02d" % (i * j))
+                edge.params.device_mac.setValue("3C:A0:12:34:56:78")#TODO
+                edge.params.device_addr.setValue("1.2.3.4")#TODO
+        '''
         
+        self.app = App()
+    
     def start(self):
-        pass
+        self.addAddress()
+        self.edge.params.port.setValue( self.localAddress() )
+        self.edge.run(ClientSim.CONFIG.path)
+    
+    def stop(self):
+        self.edge.stop()
+        self.deleteAddress()
 
--- src/server/serversim.py
+++ src/server/serversim.py
@@ -2,11 +2,31 @@
 Simulated server
 '''
 
+from cmn.nodesim import NodeSim
+from server.servercfg import ServerConfig
 from server.supernode import Supernode
+from utils.net import int2ip
 
 
-class ServerSim(object):
+class ServerSim(NodeSim):
     
-    def __init__(self):
+    CONFIG = ServerConfig()
+    
+    def __init__(self, ip, port, mport, super_addr=None):
+        NodeSim.__init__(self, ip, port, mport)
         self.supernode = Supernode("")
+        if super_addr:
+            ip, port = super_addr
+            addr = "%s:%d" % (int2ip(ip), port)
+            self.supernode.params.supernode.setValue(addr)
+    
+    def start(self):
+        self.addAddress()
+        self.supernode.params.port.setValue( self.localAddress() )
+        self.supernode.params.mgmt_port.setValue( self.mgmtAddress() )
+        self.supernode.run(ServerSim.CONFIG.path)
+    
+    def stop(self):
+        self.supernode.stop()
+        self.deleteAddress()
 
