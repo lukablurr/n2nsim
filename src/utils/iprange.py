0cb1f0b76bb88f137f219767034e0b6325bd5b43
lukablurr <lukablurr@gmail.com>
2013-11-03 17:37:31 +0200

    netmask and broadcast

--- src/utils/iprange.py
+++ src/utils/iprange.py
@@ -6,6 +6,18 @@ class IpRange(object):
     def __init__(self, start, end):
         self.start = start
         self.end = end
+        
+    def netmask(self):
+        xor = self.start ^ self.end
+        bit = xor.bit_length()
+        val = 0
+        while bit:
+            val = (val << 1) | 1
+            bit -= 1
+        return (val ^ 0xffffffff)
+    
+    def broadcast(self):
+        return (self.start | self.end)
     
     def toString(self):
         return ("%s-%s" % (int2ip(self.start), int2ip(self.end)))
